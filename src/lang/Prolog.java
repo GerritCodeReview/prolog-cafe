3248e0ac735d717a9f33ea95a6a555462e828fe5
Shawn Pearce <sop@google.com>
2015-03-13 16:55:52 -0700

    Remove unused imports
    Change-Id: I52bde22d628d5ef9096a62e812e3ba6817a7f5fa

--- src/lang/Prolog.java
+++ src/lang/Prolog.java
@@ -2,19 +2,9 @@ package com.googlecode.prolog_cafe.lang;
 import com.googlecode.prolog_cafe.exceptions.PermissionException;
 import com.googlecode.prolog_cafe.exceptions.SystemException;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.io.PushbackReader;
-import java.io.Reader;
-import java.io.Writer;
 import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.IdentityHashMap;
-import java.util.Map;
 /**
  * Prolog engine.
  *
